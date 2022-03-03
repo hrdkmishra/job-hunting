32841d86954139d0b35054e246933f22cec59229
Cicada313 <87580981+Cicada313@users.noreply.github.com>
2022-03-03 22:00:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+
+print('skills you are unfamilar with:')
+# unfamiliar_skills = input('--->')
+unfamiliar_skills = list(map(str,input('->').strip().split()))
+print(f'filtering out {unfamiliar_skills} skills...')
+
+def find_job():
+    html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=Python&txtLocation=').text
+    soup = BeautifulSoup(html_text,'lxml')
+    jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+    for job in jobs:
+        published_date = job.find('span', class_='sim-posted').span.text
+        if 'few' in published_date:
+            company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ','')
+            skills = job.find('span', class_='srp-skills').text.replace(' ','')      
+            more_info = job.header.h2.a['href']
+            if unfamiliar_skills not in skills:
+                print(f"Company name: {company_name.strip()}")
+                print(f"Required skills: {skills.strip()}")
+                print(f"More Info: {more_info}")
+                print('') 
+
+if __name__ == '__main__':
+    while True:
+        find_job()
+        time_wait = 10
+        time.sleep(time_wait * 60)
