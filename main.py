e398b6aa98e98b3b348ee179709712b22e440a1a
Cicada313 <hardikmishra0509@gmail.com>
2022-03-04 14:44:54 +0530

    output format

--- main.py
+++ main.py
@@ -5,25 +5,25 @@ import time
 print('skills you are unfamilar with(in CAPS):')
 unfamiliar_skills = input('-->')
 print(f'filtering out {unfamiliar_skills} skills...')
-
 def find_job():
     html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=Python&txtLocation=').text
     soup = BeautifulSoup(html_text,'lxml')
     jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
-    for job in jobs:
+    for index , job in enumerate(jobs):
         published_date = job.find('span', class_='sim-posted').span.text
         if 'few' in published_date:
             company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ','')
             skills = job.find('span', class_='srp-skills').text.replace(' ','')      
             more_info = job.header.h2.a['href']
             if unfamiliar_skills not in skills.upper():
-                print(f"Company name: {company_name.strip()}")
-                print(f"Required skills: {skills.strip()}")
-                print(f"More Info: {more_info}")
-                print('') 
-
+                with open(f'posts/{index}.txt','w') as f:
+                    f.write(f"Company name: {company_name.strip()} \n")
+                    f.write(f"Required skills: {skills.strip()}\n")
+                    f.write(f"More Info: {more_info}")
+                print(f'file saved:{index}')
 if __name__ == '__main__':
     while True:
         find_job()
         time_wait = 10
+        print(f'waiting {time_wait} minutes')
         time.sleep(time_wait * 60)
